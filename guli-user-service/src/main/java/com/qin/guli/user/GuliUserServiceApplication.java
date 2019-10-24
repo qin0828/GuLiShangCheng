423c0e3785c68bb71b624153bda2bbe6b3344a2e
qin0828 <18883278836@163.com>
2019-10-24 22:25:44 +0800

    第二次提交

---
+++ guli-user-service/src/main/java/com/qin/guli/user/GuliUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qin.guli.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuliUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuliUserServiceApplication.class, args);
+    }
+
+}
